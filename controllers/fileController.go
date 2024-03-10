d9a0b54f5a8ae0145166238147a8dcad6711f4ef
Sunnyraj84348 <sunnyraj7461@gmail.com>
2024-03-10 17:57:55 +0530

    api: Handle uploaded file
    Signed-off-by: Sunnyraj84348 <sunnyraj7461@gmail.com>

--- controllers/fileController.go
+++ controllers/fileController.go
@@ -1,9 +1,31 @@
 package controllers
 
 import (
+	"io"
+	"net/http"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
 )
 
 func UploadFile(ctx *gin.Context) {
+	fileHeader, err := ctx.FormFile("file")
+	if err != nil {
+		ctx.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
 
+	fileName := filepath.Base(fileHeader.Filename)
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
 }
