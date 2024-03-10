03c2ec1654c98c26590edc9c138a5ebe24998215
Sunnyraj84348 <sunnyraj7461@gmail.com>
2024-03-10 17:52:59 +0530

    go: Create /upload endpoint
    Signed-off-by: Sunnyraj84348 <sunnyraj7461@gmail.com>

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"SentinelVault/controllers"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	router := gin.Default()
+
+	router.POST("/upload", controllers.UploadFile)
+
+	err := router.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
